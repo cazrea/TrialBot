e60143d98d4d964d805bde2d880ce06d5e7e31b6
Robin Recato <praincingtoad@gmail.com>
2022-06-02 22:51:56 +0800

    shop

---
+++ storeitems.js
@@ -0,0 +1,7 @@
+module.exports = [
+        {name: 'Leaves', price: 5, food: 1},
+        {name: 'Berries', price: 10, food: 3},
+        {name: 'Fruit', price: 12, food: 7},
+        {name: 'Salad', price: 15, food: 10},
+        {name: 'Meal', price: 20, food: 20},
+]

f9d2b25a2660ece79eedf655171f0959085774c7
Robin Recato <praincingtoad@gmail.com>
2022-06-02 18:46:53 +0800

    deposit

--- commands/dMBC.js
+++ commands/dMBC.js
@@ -1,25 +1,26 @@
 const { MessageEmbed, User } = require("discord.js");
-const messageCreate = require("../events/guild/messageCreate");
 const profileModel = require("../models/profileSchema");
 const ms = require('ms');
 
 module.exports = {
     name: 'dMBC',
     aliases: ['dM'],
-    async execute(message, args, cmd, client, Discord, profileData) {
+    async execute(message, args, cmd, client, discord, profileData) {
         const amount = args[0];
 
-        if(amount % 1 != 0 || amount <=0) return 
-            const notAWholeNumEmbed = new MessageEmbed()
-                .setColor('#800020')
-                .setTitle('Oops! The transaction went wrong!')
-                .setDescription('The number must be a whole number!')
-                .setFooter({text: 'Do ~d MBC # again'});
+        if (amount % 1 != 0 || amount <=0) 
+            return 
+                const notAWholeNumEmbed = new MessageEmbed()
+                    .setColor('#800020')
+                    .setTitle('Oops! The transaction went wrong!')
+                    .setDescription('The number must be a whole number!')
+                    .setFooter({text: 'Do ~d MBC # again'});
 
-            message.channel.send({embeds: [notAWholeNumEmbed]});
+                message.channel.send({embeds: [notAWholeNumEmbed]});
 
         try {
-            if(amount > profileData.MBC) return
+            if (amount > profileData.MBC) 
+            return
                 const notEnoughEmbed = new MessageEmbed()
                     .setColor('#800020')
                     .setTitle(`Oh no, you don't have that many micro brain cells!`)
@@ -28,23 +29,23 @@ module.exports = {
 
                 message.channel.send({embeds: [notEnoughEmbed]});
 
-                await profileModel.findOneAndUpdate({
+            await profileModel.findOneAndUpdate(
+                {
                     userID: message.author.id
-                }, {
+                }, 
+                {
                     $inc: {
                         MBC: -amount,
                         MBBank: amount,
-                    },
-                    
-                    });
-
+                        }
+                });  
                 const sdMBCEmbed = new MessageEmbed()
                     .setColor('#CD7F32')
                     .setTitle(`Congrats!`)
                     .setDescription(`You've successfully deposited ${amount} Micro Brain Cells into the Brain Bank!`)
                     .setFooter({text: 'Check your ~balance to confirm.'});
 
-                message.channel.send({embeds: [sdMBCEmbed]});
+                return message.channel.send({embeds: [sdMBCEmbed]});
 
 
         } catch(err) {

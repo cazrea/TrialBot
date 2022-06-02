0d7a811ebce393c4611f2641b2f0a91f9b1f5a89
Robin Recato <praincingtoad@gmail.com>
2022-06-02 16:55:52 +0800

    Test

--- models/profileSchema.js
+++ models/profileSchema.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const profileSchema = new mongoose.Schema({
     userID: {type: String, require: true, unique: true},
     serverID: {type: String, require: true},
-    MBC: {type: Number, default: 500},
     BrainCells: {type: Number, default: 1},
     bank: {type: Number},
+    MBC: {type: Number, default: 500},
     MBBank: {type: Number},
     food: {type: Number, default: 0}
 });
